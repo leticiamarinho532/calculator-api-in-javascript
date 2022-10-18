250c59b874ff0e479e7408592f7f7a1ea8dd4a15
Leticia <leticia.marinho532@gmail.com>
2022-10-18 17:16:11 -0300

    feat: create calculator service with basic operations

---
+++ src/services/calculator.js
@@ -0,0 +1,31 @@
+class calculatorService {
+    add(firstNumber, secondNumber) 
+    {
+        let result = firstNumber + secondNumber;
+
+        return result;
+    }
+
+    subtract(firstNumber, secondNumber) 
+    {
+        let result = firstNumber - secondNumber;
+
+        return result;
+    }
+
+    divide(firstNumber, secondNumber) 
+    {
+        let result = firstNumber / secondNumber;
+
+        return result;
+    }
+
+    multiply(firstNumber, secondNumber)
+    {
+        let result = firstNumber * secondNumber;
+
+        return result;
+    }
+}
+
+module.exports = calculatorService;
